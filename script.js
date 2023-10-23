ab9365a4c30eae3cc881f166011ba9e52aa9a9e5
Sayali Nikam <137673498+SayaliGN@users.noreply.github.com>
2023-10-23 08:34:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const button = document.getElementByTagName("input");
+const myFun = function removeColor() {
+	var x = document.getElementById("colorSelect");
+	x.remove(x.selectedIndex);	
+}
+button.addEventListener('click',myFun);
